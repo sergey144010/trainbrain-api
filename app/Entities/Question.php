d5aa4b5d296458ce76befc45ea593cb15c43a8cb
sergey <sergey.i@pssltd.ru>
2022-02-09 21:38:37 +0300

    [master] Question logic

--- app/Entities/Question.php
+++ app/Entities/Question.php
@@ -4,19 +4,26 @@ namespace App\Entities;
 
 class Question
 {
-    public const DID_NOT_DECIDED = null;
-    public const DECIDED_WRONG = 0;
-    public const DECIDED_RIGHT = 1;
+    public const STATUS_DID_NOT_DECIDED = null;
+    public const STATUS_DECIDED_WRONG = 0;
+    public const STATUS_DECIDED_RIGHT = 1;
+
+    public const STATE_KEY_QUESTION = 'question';
+    public const STATE_KEY_ANSWERS = 'answers';
+    public const STATE_KEY_TRUE_ID = 'true_id';
+    public const STATE_KEY_STATUS = 'status';
+    public const STATE_KEY_CURRENT = 'current';
 
     /** @var Array<Definition> $definitionCollection */
     private array $definitionCollection = [];
     private array $state = [
-        'question' => '',
-        'answers' => [],
-        'status' => null,
-        'current' => false,
-        'true_id' => null,
+        self::STATE_KEY_QUESTION => '',
+        self::STATE_KEY_ANSWERS => [],
+        self::STATE_KEY_TRUE_ID => null,
     ];
+    private bool|null $status = null;
+    private bool $current = false;
+
 
     public function addDefinition(Definition $definition): void
     {
@@ -31,19 +38,23 @@ class Question
 
     private function initState(int $key): void
     {
-        $this->state['question'] = $this->definitionCollection[$key]->definition();
-        $this->state['true_id'] = $key;
-        $this->state['answers'] = array_map(function ($definition) {
+        $this->state[self::STATE_KEY_QUESTION] = $this->definitionCollection[$key]->definition();
+        $this->state[self::STATE_KEY_TRUE_ID] = $key;
+        $this->state[self::STATE_KEY_ANSWERS] = array_map(function ($definition) {
             return $definition->name();
         }, $this->definitionCollection);
     }
 
     public function state(): array
     {
-        if (empty($this->state['question'])) {
+        if (empty($this->state[self::STATE_KEY_QUESTION])) {
             $this->make();
         }
 
-        return $this->state;
+        return [
+            ...$this->state,
+            self::STATE_KEY_STATUS => $this->status,
+            self::STATE_KEY_CURRENT => $this->current,
+        ];
     }
 }
--- tests/Units/QuestionTest.php
+++ tests/Units/QuestionTest.php
@@ -1,19 +1,29 @@
 <?php
+
+use App\Entities\Definition;
+use App\Entities\Question;
 use PHPUnit\Framework\TestCase;
 
 class QuestionTest extends TestCase
 {
-    public function testMakeState()
+    public function testMakeDefaultState()
     {
-        $definition1 = new \App\Entities\Definition(1, 'one', 'OneDefinition');
-        $definition2 = new \App\Entities\Definition(2, 'two', 'TwoDefinition');
-        $definition3 = new \App\Entities\Definition(3, 'three', 'ThreeDefinition');
+        $definition1 = new Definition(1, 'one', 'OneDefinition');
+        $definition2 = new Definition(2, 'two', 'TwoDefinition');
+        $definition3 = new Definition(3, 'three', 'ThreeDefinition');
 
-        $question = new \App\Entities\Question();
+        $question = new Question();
         $question->addDefinition($definition1);
         $question->addDefinition($definition2);
         $question->addDefinition($definition3);
 
-        self::assertEquals(['qwe'=>123], $question->state());
+        self::assertArrayHasKey(Question::STATE_KEY_QUESTION, $question->state());
+        self::assertArrayHasKey(Question::STATE_KEY_ANSWERS, $question->state());
+        self::assertArrayHasKey(Question::STATE_KEY_TRUE_ID, $question->state());
+        self::assertArrayHasKey(Question::STATE_KEY_STATUS, $question->state());
+        self::assertArrayHasKey(Question::STATE_KEY_CURRENT, $question->state());
+
+        self::assertEquals(null, $question->state()[Question::STATE_KEY_STATUS]);
+        self::assertEquals(false, $question->state()[Question::STATE_KEY_CURRENT]);
     }
 }
