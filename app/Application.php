04482ea71de70aa76c0335165948499b870a8b9c
sergey <sergey.i@pssltd.ru>
2022-05-24 01:43:42 +0300

    Dev environment

---
+++ app/Application.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Application
+{
+    public function __construct()
+	{
+		echo 'Hello';
+    }
+}
---
+++ app/Repositories/DefinitionRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class DefinitionRepository implement DefinitionRepositoryInterface
+{
+	# from mongo
+    public function __construction()
+	{
+		# init mongo client
+		# https://www.mongodb.com/docs/php-library/current/tutorial/install-php-library/
+		# https://www.mongodb.com/docs/php-library/current/tutorial/crud/
+	}
+}
